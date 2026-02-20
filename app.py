6491c94d63818ce34cc0cef6d46c20ef25358ab4
Devon <ds.pydev@gmail.com>
2026-02-20 18:51:07 +0200

    Fix issue with viewing uploaded med docs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session, send_file
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from functools import wraps
